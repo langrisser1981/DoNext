6349e4cdf09c13f53f1ab725b79cdce4c80fcd9e
lenny.cheng <lenny.cheng@linecorp.com>
2025-07-09 16:32:47 +0800

    Initial Commit

---
+++ DoNextTests/DoNextTests.swift
@@ -0,0 +1,16 @@
+//
+//  DoNextTests.swift
+//  DoNextTests
+//
+//  Created by Lenny Cheng on 2025/7/9.
+//
+
+import Testing
+
+struct DoNextTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
